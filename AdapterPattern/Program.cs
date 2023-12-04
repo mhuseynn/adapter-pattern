da2427dfe9f0bd9d901f50541fcc6b2efca9b1a7
mhuseynn <hmehdiyev2005@gmail.com>
2023-12-04 20:13:29 +0400

    firs

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Adapter
+{
+  
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving in normal weather");
+        }
+    }
+
+    
+    public interface IRainyWeatherAdapter
+    {
+        void DriveInRainyWeather();
+    }
+
+    
+    public class RainyWeatherAdapter : IRainyWeatherAdapter
+    {
+        private  ICar car;
+
+        public RainyWeatherAdapter(ICar car)
+        {
+            this.car = car;
+        }
+
+        public void DriveInRainyWeather()
+        {           
+            Console.WriteLine("Adapter for Driving in Rainy weather");
+           
+        }
+    }
+
+   
+
+
+    class Program
+    {
+        static void Main()
+        {
+            
+            ICar car = new Car();
+          
+            //Normal Weather
+            car.Drive();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //For Rainy Weather
+            IRainyWeatherAdapter modied_car = new RainyWeatherAdapter(car);
+
+            modied_car.DriveInRainyWeather();
+        }
+    }
+
+}
